aeb173fd8a473d275bbbb243c7cdb8889319e120
manivannan <manivannan@dl.com>
2020-11-25 16:00:34 +0530

    v2.0 releaseed

--- convert_spacy_train_data.py
+++ convert_spacy_train_data.py
@@ -1,6 +1,6 @@
 import json
 
-filename = input("Enter your train data filename : ")
+filename = input("Enter your train data filename(json) : ")
 print(filename)
 
 
@@ -9,19 +9,20 @@ with open(filename) as train_data:
 
 TRAIN_DATA = []
 for data in train:
-    ents = [tuple(entity[:3]) for entity in data['entities']]
+	ents = [tuple(entity[:3]) for entity in data['entities']]
 	TRAIN_DATA.append((data['content'],{'entities':ents}))
 
 
 with open('{}'.format(filename.replace('json','txt')),'w') as write:
 	write.write(str(TRAIN_DATA))
 
-print('-------------Copy and Paste to spacy training-------------')
-print()
-print()
-print()
-print(TRAIN_DATA)
-print()
-print()
-print()
-print('--------------------------End-----------------------------')
+print("[INFO] Stored the spacy training data and  filename is {}".format(filename.replace('json','txt')))
+# print('-------------Copy and Paste to spacy training-------------')
+# print()
+# print()
+# print()
+# print(TRAIN_DATA)
+# print()
+# print()
+# print()
+# print('--------------------------End-----------------------------')
